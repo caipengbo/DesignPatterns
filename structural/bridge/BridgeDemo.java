155288a56ed899f026d3c3d7531d9fdb9b66932b
Myth <caipengbo@outlook.com>
2019-12-04 10:35:49 +0800

    Bridge Pattern

---
+++ structural/bridge/BridgeDemo.java
@@ -0,0 +1,153 @@
+package structural.bridge;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+/**
+ * Title: 桥接模式
+ * Desc:  跨平台图像浏览系统
+ * Created by Myth-Lab on 12/4/2019
+ */
+/*
+Sunny软件公司欲开发一个跨平台图像浏览系统，要求该系统能够显示BMP、JPG、GIF、PNG等多种格式的文件，
+并且能够在Windows、Linux、Unix等多个操作系统上运行。
+系统首先将各种格式的文件解析为像素矩阵(Matrix)，然后将像素矩阵显示在屏幕上，在不同的操作系统中可以调用不同的绘制函数来绘制像素矩阵。
+系统需具有较好的扩展性以支持新的文件格式和操作系统。（使用xml进行配置）
+ */
+// 多继承思路：Image， 多个FormatImage继承Image，PlatformImpl继承各个FormatImage，类爆炸
+// 桥接模式：将文件格式和操作系统
+//像素矩阵类：辅助类，各种格式的文件最终都被转化为像素矩阵，不同的操作系统提供不同的方式显示像素矩阵
+class Matrix {
+    //此处代码省略
+}
+
+//抽象图像类：抽象类
+abstract class Image {
+    protected ImageImp imp;
+
+    public void setImageImp(ImageImp imp) {
+        this.imp = imp;
+    }
+
+    public abstract void parseFile(String fileName);
+}
+// 第一个维度
+//JPG格式图像：扩充抽象类
+class JPGImage extends Image {
+    public void parseFile(String fileName) {
+        // Parse JPG file
+        Matrix m = new Matrix();
+        imp.doPaint(m);  // 调用实现接口的方法
+        System.out.println(fileName + ", JPG format.");
+    }
+}
+
+//PNG格式图像：扩充抽象类
+class PNGImage extends Image {
+    public void parseFile(String fileName) {
+        // Parse PNG file
+        Matrix m = new Matrix();
+        imp.doPaint(m);
+        System.out.println(fileName + ", PNG format.");
+    }
+}
+
+//BMP格式图像：扩充抽象类
+class BMPImage extends Image {
+    public void parseFile(String fileName) {
+        // Parse BMP file
+        Matrix m = new Matrix();
+        imp.doPaint(m);
+        System.out.println(fileName + ", BMP format.");
+    }
+}
+
+//GIF格式图像：扩充抽象类
+class GIFImage extends Image {
+    public void parseFile(String fileName) {
+        // Parse GIF file
+        Matrix m = new Matrix();
+        imp.doPaint(m);
+        System.out.println(fileName + ", GIF format.");
+    }
+}
+// 第二个维度
+//抽象操作系统实现类：实现类接口
+interface ImageImp {
+    public void doPaint(Matrix m);  //显示像素矩阵m
+}
+//Windows操作系统实现类：具体实现类
+class WindowsImp implements ImageImp {
+    public void doPaint(Matrix m) {
+        //调用 Windows系统的绘制函数绘制像素矩阵
+        System.out.print("Use Windows system call paint image!");
+    }
+}
+
+//Linux操作系统实现类：具体实现类
+class LinuxImp implements ImageImp {
+    public void doPaint(Matrix m) {
+        //调用Linux系统的绘制函数绘制像素矩阵
+        System.out.print("Use Linux system call paint image!");
+    }
+}
+
+//Unix操作系统实现类：具体实现类
+class UnixImp implements ImageImp {
+    public void doPaint(Matrix m) {
+        //调用Unix系统的绘制函数绘制像素矩阵
+        System.out.print("Use Unix system call paint image!");
+    }
+}
+// 解析xml文件
+class XMLUtil {
+    //该方法用于从XML配置文件中提取具体类类名，并返回一个实例对象
+    public static Object getBean(String args) {
+        try {
+            //创建文档对象
+            DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = dFactory.newDocumentBuilder();
+            Document doc;
+            doc = builder.parse(new File("structural/bridge/config.xml"));
+            NodeList nl=null;
+            Node classNode=null;
+            String cName = null;
+            nl = doc.getElementsByTagName("className");
+
+            if(args.equals("image")) {
+                //获取第一个包含类名的节点，即扩充抽象类
+                classNode=nl.item(0).getFirstChild();
+
+            }
+            else if(args.equals("os")) {
+                //获取第二个包含类名的节点，即具体实现类
+                classNode = nl.item(1).getFirstChild();
+            }
+            assert classNode != null;
+            cName = classNode.getNodeValue();
+            //通过类名生成实例对象并将其返回
+            Class c = Class.forName(cName);
+            return c.getDeclaredConstructor().newInstance();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+public class BridgeDemo {
+    public static void main(String[] args) {
+        Image image;
+        ImageImp imp;
+        image = (Image)XMLUtil.getBean("image");
+        imp = (ImageImp)XMLUtil.getBean("os");
+        assert image != null;
+        image.setImageImp(imp);
+        image.parseFile("test");
+    }
+}
--- structural/bridge/ShapeColorDemo.java
+++ structural/bridge/ShapeColorDemo.java
@@ -5,65 +5,56 @@ package structural.bridge;
  * Desc: 为形状填充颜色
  * Created by Myth-Lab on 11/29/2019
  */
-interface Shape {
-    void draw();
-}
-class Rectangle implements Shape {
-    @Override
-    public void draw() {
-        System.out.println("绘制矩形");
-    }
-}
-class Round implements Shape {
-    @Override
-    public void draw() {
-        System.out.println("绘制圆形");
-    }
-}
-// ********** 增加颜色特性
-interface Color {
-    String getColor();
-}
-
-class Red implements Color {
-    @Override
-    public String getColor() {
-        return "红";
-    }
-}
-class Blue implements Color {
-    @Override
-    public String getColor() {
-        return "蓝";
-    }
-}
-// 使用桥连方式扩展原来的接口
-abstract class IShape {
-    // 将Color桥连进去
-    Color color;
-    public void fill(Color color) {
+abstract class Shape {
+    protected Color color;
+    public void setColor(Color color) {
         this.color = color;
     }
     abstract void draw();
 }
-class RectangleNew extends IShape {
-    // 实现
+// 第一个维度：扩充对象
+class Rectangle extends Shape {
     @Override
     public void draw() {
-        System.out.println("绘制矩形，" + "填充" + color.getColor());
+        System.out.println("Draw Rectangle");
+        color.fill();
     }
 }
-class RoundNew extends IShape {
+class Round extends Shape {
     @Override
     public void draw() {
-        System.out.println("绘制圆形，" + "填充" + color.getColor());
+        System.out.println("Draw Round");
+        color.fill();
     }
 }
+// 第二个维度：实现接口
+interface Color {
+    void fill();
+}
+
+class Red implements Color {
+    @Override
+    public void fill() {
+        System.out.println("Fill red color");
+    }
+}
+class Blue implements Color {
+    @Override
+    public void fill() {
+        System.out.println("Fill blue color");
+    }
+}
+
 /*
 将抽象部分与它的实现部分分离，使它们都可以独立地变化。抽象部分指的是父类，对应本例中的形状类，
 实现部分指的是不同子类的区别之处。将子类的区别方式 —— 也就是本例中的颜色 —— 分离成接口，
 通过组合的方式桥接颜色和形状，这就是桥接模式，它主要用于 两个或多个同等级的接口。
  */
 public class ShapeColorDemo {
-
+    public static void main(String[] args) {
+        Shape shape = new Rectangle();
+        Color color = new Red();
+        shape.setColor(color);
+        shape.draw();
+    }
 }
